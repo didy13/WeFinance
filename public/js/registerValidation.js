455fbcda2677cadfaf4c6ad6bea18b2268ba2e0d
Aleksa Stankic <bystankic@gmail.com>
2025-09-06 17:29:54 +0200

    Popavljen CSS i popavljena validacija, takodje sam uradio bolji izgled errora.

--- public/js/registerValidation.js
+++ public/js/registerValidation.js
@@ -3,16 +3,16 @@ const { body } = require('express-validator');
 const registerValidation = [
     body('username')
         .notEmpty()
-        .withMessage('Username je obavezan')
+        .withMessage('Korisničko ime je obavezno!')
         .isLength({ min: 3 })
-        .withMessage('Username mora imati najmanje 3 karaktera'),
+        .withMessage('Korisničko ime mora da ima najmanje 3 karaktera!'),
     body('password')
         .notEmpty()
-        .withMessage('Password je obavezan')
+        .withMessage('Lozinka je obavezna!')
         .isLength({ min: 6 })
-        .withMessage('Password mora imati najmanje 6 karaktera')
+        .withMessage('Lozinka mora da ima najmanje 6 karaktera!')
         .matches(/(?=.*[A-Z])(?=.*[0-9])/)
-        .withMessage('Lozinka mora da ima bar jedno veliko slovo i bar jedan broj'),
+        .withMessage('Lozinka mora da ima bar jedno veliko slovo i bar jedan broj!'),
 ];
 
 module.exports = registerValidation;
